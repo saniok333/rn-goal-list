84aaafc0a67c891ab22f58fe6192fbdf9b37ae2d
saniok333 <alex.o.csc@gmail.com>
2022-07-02 18:58:52 +0300

    Added inline styles

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ marginBottom: 30 }}>
+        Open up App.js to start working on your app!!
+      </Text>
+      <Button title="Tap me" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
